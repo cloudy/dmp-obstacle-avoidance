036c087d5e061ba0f010db30faff298ea72a0742
cloudy <im@joe.cloud>
2019-03-28 21:08:08 -0500

    Static obstacles in test

--- dmp_obstacle_avoidance.py
+++ dmp_obstacle_avoidance.py
@@ -2,7 +2,6 @@
 
 #TODO: Seperate DMP for position and orientation
 
-import argparse
 from pyrdmp.dmp import DynamicMovementPrimitive as DMP
 import pyrdmp.plots as plot
 from pyrdmp.utils import *
@@ -11,6 +10,111 @@ import sympy as sp
 from sawyer_kinematics import Sawyer
 import rospy
 import intera_interface
+import argparse
+
+
+def main(arg):
+    rospy.init_node("Sawyer_DMP")
+    limb = intera_interface.Limb('right')
+    
+    position_dmp = DMP(arg.gain, arg.num_gaussians, arg.stabilization, arg.obstacle_avoidance)
+    orientation_dmp = DMP(arg.gain, arg.num_gaussians, arg.stabilization, False) # no obstacle avoidance on orientation
+
+    data = load_demo(arg.input_file)
+    t, q = parse_demo(data)
+
+    s = position_dmp.phase(t)
+    psv = position_dmp.distributions(s)
+    t = normalize_vector(t)
+    
+    print('Calculating forward kinematics...')
+    robot = Sawyer()
+    Te = sp.lambdify(robot.q, robot.get_T_f()[-1])
+    ht = np.array([Te(*robot.JointOffset(qi)) for qi in q])
+    position, orientation = np.zeros((len(t), 3)), np.zeros((len(t), 3))
+
+    for i, h in enumerate(ht):
+        c = h[:3,3]
+        p = np.arctan2(-h[2,0], np.sqrt(h[0,0]**2 + h[1,0]**2))
+        y = np.arctan2(h[1,0]/np.cos(p), h[0,0]/np.cos(p))
+        r = np.arctan2(h[2,1]/np.cos(p), h[2,2]/np.cos(p))
+        
+        if i > 0:
+            y = fix_angle(orientation[i-1,2], y)
+            p = fix_angle(orientation[i-1,1], p)
+            r = fix_angle(orientation[i-1,0], r)
+
+        position[i] = c
+        orientation[i] = np.stack((r,p,y))
+    
+    
+    # Set static obstacles
+    #obstacles = np.array([np.concatenate((position[len(position)//3], orientation[len(position)//3]))])#,:3]])
+    #obstacles = np.array([np.random.uniform(l,h,size=(2,)) for l,h in [(-0.2,0.6),(-0.6,0.2),(-0.1,0.2),(0,0),(0,0),(0,0)]]).T
+    ob_angles = [-1.931447265625, -0.3394013671875, 0.4098779296875, 1.296037109375, -0.550625, 0.7161005859375, 0.5338125] 
+    obstacles = np.array([np.concatenate((Te(*robot.JointOffset(ob_angles))[:3,3], [0,0,0]))])
+    #obstacles = np.array([[0.4134, -0.5456, 0.109, 0, 0, 0]])
+    obstacles = np.array([[ 0.2, -0.69431369, -0.05,0,0,0]])#-0.08348392, 0,0,0]] 
+
+    print('Smoothing and filtering cartesian trajectory...')
+    dposition, ddposition = np.zeros((2, position.shape[0], position.shape[1]))
+    for i in range(position.shape[1]):
+        position[:, i] = smooth_trajectory(position[:, i], arg.window)
+        dposition[:, i] = vel(position[:, i], t)
+        ddposition[:, i] = vel(dposition[:, i], t)
+    
+    dorientation, ddorientation = np.zeros((2, orientation.shape[0], orientation.shape[1]))
+    for i in range(position.shape[1]):
+        orientation[:, i] = smooth_trajectory(orientation[:, i], arg.window)
+        dorientation[:, i] = vel(orientation[:, i], t)
+        ddorientation[:, i] = vel(dorientation[:, i], t)
+
+    # Filter the position velocity and acceleration signals
+    f_position, f_dposition, f_ddposition = np.zeros((3, position.shape[0], position.shape[1]))
+    for i in range(position.shape[1]):
+        f_position[:, i] = blend_trajectory(position[:, i], dposition[:, i], t, arg.blends)
+        f_dposition[:, i] = vel(f_position[:, i], t)
+        f_ddposition[:, i] = vel(f_dposition[:, i], t)
+    
+    f_orientation, f_dorientation, f_ddorientation = np.zeros((3, orientation.shape[0], orientation.shape[1]))
+    for i in range(position.shape[1]):
+        f_orientation[:, i] = blend_trajectory(orientation[:, i], dorientation[:, i], t, arg.blends)
+        f_dorientation[:, i] = vel(f_orientation[:, i], t)
+        f_ddorientation[:, i] = vel(f_dorientation[:, i], t)
+
+    print('DMP: Imitating trajectory...')
+    ftarget_position, w_position = position_dmp.imitate(position, dposition, ddposition, t, s, psv)
+    ftarget_orientation, w_orientation = orientation_dmp.imitate(orientation, dorientation, ddorientation, t, s, psv)
+    
+    print('DMP: Generating trajectory...')
+    ddxp, dxp, xp = position_dmp.generate(w_position, f_position[0], f_position[-1], t, s, psv, obstacles)
+    ddxo, dxo, xo = position_dmp.generate(w_orientation, f_orientation[0], f_orientation[-1], t, s, psv, obstacles)
+
+    print('Calculating inverse kinematics from DMP-cartesian trajectory...')
+    xc = np.zeros(q.shape)
+    prev_sol = q[0]
+    for i,xi in enumerate(xp):
+        if i > 0:
+            prev_sol = xc[i - 1]
+
+        ik = robot.Inverse_Kinematics(limb, xi, np.rad2deg(xo[i]), prev_sol )
+        xc[i] = [ik[j] for j in limb.joint_names()]
+
+    real_sol = np.concatenate((xp,xo),axis=1)
+    gen_sol = np.concatenate((position,orientation), axis=1)
+   
+    if arg.show_plots:
+        #plot.comparison(t, q, xc, None, labels=['Original q', 'cartesian-DMP', 'None'])
+        #plot.position(t, xo, position[:,3:], title='Orientation')
+        plot.cartesian_history([real_sol, gen_sol, obstacles], [0.2,0.2,100.0])
+        plot.show_all()
+
+    print('Saving joint angle solutions to: %s' % (arg.output_file + '_trajectory.txt'))
+    traj_final = np.concatenate((xc, np.multiply(np.ones((xc.shape[0], 1)), 0.0402075604203)), axis=1)
+    time = np.linspace(0, t[-1], xc.shape[0]).reshape((xc.shape[0], 1))
+    traj_final = np.concatenate((t.reshape((-1, 1)), traj_final), axis=1)
+    header = 'time,right_j0,right_j1,right_j2,right_j3,right_j4,right_j5,right_j6,right_gripper'
+    np.savetxt(arg.output_file + '_trajectory.txt', traj_final, delimiter=',', header=header, comments='', fmt="%1.12f")
 
 
 def fix_angle(previous, current):
@@ -24,104 +128,11 @@ def fix_angle(previous, current):
     return current
 
 
-def main(arg):
-    rospy.init_node("Sawyer_DMP")
-    limb = intera_interface.Limb('right')
-    
-    # Initialize the DMP class
-    my_dmp = DMP(arg.gain, arg.num_gaussians, arg.stabilization, arg.obstacle_avoidance)
-
-    # Load the demo data
-    data = load_demo(arg.input_file)
-
-    # Obtain the joint position data and the time vector
-    t, q = parse_demo(data)
-
-    # Get the phase from the time vector
-    s = my_dmp.phase(t)
-
-    # Get the Gaussian
-    psv = my_dmp.distributions(s)
-
-    # Normalize the time vector
-    t = normalize_vector(t)
-    
-    print('Calculating forward kinematics...')
-    robot = Sawyer()
-    Te = sp.lambdify(robot.q, robot.get_T_f()[-1])
-    ht = np.array([Te(*robot.JointOffset(qi)) for qi in q])
-    pose, orientation = np.zeros((len(t), 6)), np.zeros((len(t), 3))
-
-    for i, h in enumerate(ht):
-        c = h[:3,3]
-        p = np.arctan2(-h[2,0], np.sqrt(h[0,0]**2 + h[1,0]**2))
-        y = np.arctan2(h[1,0]/np.cos(p), h[0,0]/np.cos(p))
-        r = np.arctan2(h[2,1]/np.cos(p), h[2,2]/np.cos(p))
-        
-        if i > 0:
-            y = fix_angle(pose[i-1,5], y)
-            p = fix_angle(pose[i-1,4], p)
-            r = fix_angle(pose[i-1,3], r)
-
-        pose[i] = np.concatenate((c,np.stack((r,p,y))))
-    
-    # Set static obstacles
-    obstacles = np.array([pose[len(pose)//2]])#,:3]])
-    
-    print('Smoothing and filtering cartesian trajectory...')
-    # Compute velocity and acceleration
-    dpose, ddpose = np.zeros((2, pose.shape[0], pose.shape[1]))
-    for i in range(pose.shape[1]):
-        pose[:, i] = smooth_trajectory(pose[:, i], arg.window)
-        dpose[:, i] = vel(pose[:, i], t)
-        ddpose[:, i] = vel(dpose[:, i], t)
-
-    # Filter the position velocity and acceleration signals
-    f_pose, f_dpose, f_ddpose = np.zeros((3, pose.shape[0], pose.shape[1]))
-
-    for i in range(pose.shape[1]):
-        f_pose[:, i] = blend_trajectory(pose[:, i], dpose[:, i], t, arg.blends)
-        f_dpose[:, i] = vel(f_pose[:, i], t)
-        f_ddpose[:, i] = vel(f_dpose[:, i], t)
-
-    print('DMP: Imitating trajectory...')
-    ftarget, w = my_dmp.imitate(pose, dpose, ddpose, t, s, psv)
-    
-    print('DMP: Generating trajectory...')
-    ddx, dx, x = my_dmp.generate(w, f_pose[0], f_pose[-1], t, s, psv, obstacles)
-
-    print('Calculating inverse kinematics from DMP-cartesian trajectory...')
-    xc = np.zeros(q.shape)
-    prev_sol = q[0]
-    for i,xi in enumerate(x):
-        if i > 0:
-            prev_sol = xc[i - 1]
-
-        ik = robot.Inverse_Kinematics(limb, xi[:3], np.rad2deg(xi[3:]), prev_sol )
-        xc[i] = [ik[j] for j in limb.joint_names()]
-
-    pose_mod = np.array([Te(*qi) for qi in xc])[:,:3,3]
-  
-    # Plot functions
-    if arg.show_plots:
-        plot.comparison(t, q, xc, None, labels=['Original q', 'cartesian-DMP', 'None'])
-        plot.position(t, x[:,3:], pose[:,3:], title='Orientation')
-        plot.cartesian_history([x[:,:3], pose[:,:3], obstacles])
-        plot.show_all()
-
-    print('Saving joint angle solutions to: %s' % (arg.output_file + '_trajectory.txt'))
-    traj_final = np.concatenate((xc, np.multiply(np.ones((xc.shape[0], 1)), 0.0402075604203)), axis=1)
-    time = np.linspace(0, t[-1], xc.shape[0]).reshape((xc.shape[0], 1))
-    traj_final = np.concatenate((t.reshape((-1, 1)), traj_final), axis=1)
-    header = 'time,right_j0,right_j1,right_j2,right_j3,right_j4,right_j5,right_j6,right_gripper'
-    np.savetxt(arg.output_file + '_trajectory.txt', traj_final, delimiter=',', header=header, comments='', fmt="%1.12f")
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Use Reinforced DMP to adapt to new goals")
     parser.add_argument('-ga', '--gain', type=float, default=20.0,
                         help="Set the gain of the DMP transformation system.")
-    parser.add_argument('-ng', '--num-gaussians', type=int, default=20,
+    parser.add_argument('-ng', '--num-gaussians', type=int, default=500,
                         help="Number of Gaussians")
     parser.add_argument('-sb', '--stabilization', type=bool, default=False,
                         help="Add a stability term at end of trajectory")
